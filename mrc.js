f5848584f26b9ad0892394b7be08348e39d9a25f
biodun-ojo <149972442+biodun-ojo@users.noreply.github.com>
2024-10-06 14:38:28 +0100

    UI feature complete

--- mrc.js
+++ mrc.js
@@ -1,1 +1,12 @@
-console.log (' hello world ')
+console.log ('lets do this')
+
+const mortgageAmount = document.getElementsByClassName('mortA')
+const mortgageTerm = document.getElementsByClassName('mortT') 
+const interestRate = document.getElementsByClassName('interRate')
+const button = document.getElementById('calculator')
+
+function output() {
+    return 2
+}
+
+button.addEventListener('click', () => console.log(output()))
