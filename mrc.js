bdda0ca7f62b7ece1d0eb0c032160b054f87958e
biodun-ojo <149972442+biodun-ojo@users.noreply.github.com>
2024-10-07 23:32:16 +0100

    update

--- mrc.js
+++ mrc.js
@@ -4,8 +4,13 @@ const mortgageAmount = document.getElementById('mortA')
 const mortgageTerm = document.getElementById('mortT') 
 const interestRate = document.getElementById('interRate')
 const button = document.getElementById('calculator')
+const monthrepay = document.getElementById('monthrepay')
+const totalrepay = document.getElementById('repaytotal')
+
 
 function output() {
+
+    //repayment type
     let rt = parseFloat(mortgageAmount.value)
     let hn = parseInt(mortgageTerm.value * 12)
     let ko = (parseFloat(interestRate.value) / 100)/12
@@ -14,6 +19,10 @@ function output() {
     //let sum = rt((ko(1+ko)**hn)/(((1+ko)**hn)-1))
 
     console.log(sum) 
+    
+
+    
+    //interest only type
 }
 
 button.addEventListener('click', () => output())
