5b95e36a7629ab7c5b6c8f3e6185a23bba19c4d6
biodun-ojo <149972442+biodun-ojo@users.noreply.github.com>
2024-10-08 12:02:23 +0100

    logic complete

--- mrc.js
+++ mrc.js
@@ -1,27 +1,52 @@
-console.log ('lets do this')
+console.log('lets do this')
 
 const mortgageAmount = document.getElementById('mortA')
-const mortgageTerm = document.getElementById('mortT') 
+const mortgageTerm = document.getElementById('mortT')
 const interestRate = document.getElementById('interRate')
 const button = document.getElementById('calculator')
 const monthrepay = document.getElementById('monthrepay')
 const totalrepay = document.getElementById('repaytotal')
+const option1 = document.getElementById('repayment')
+const option2 = document.getElementById('interestOnly')
 
 
 function output() {
 
-    //repayment type
-    let rt = parseFloat(mortgageAmount.value)
-    let hn = parseInt(mortgageTerm.value * 12)
-    let ko = (parseFloat(interestRate.value) / 100)/12
+    let principal = parseFloat(mortgageAmount.value) //principal
+    let loanTerm = parseInt(mortgageTerm.value * 12) //loan term
+    let InterestRat = (parseFloat(interestRate.value) / 100) / 12 //interest rate
 
-    let sum = Math.floor((rt * (ko * Math.pow(1 + ko, hn))) / (Math.pow(1 + ko, hn) - 1));
-    //let sum = rt((ko(1+ko)**hn)/(((1+ko)**hn)-1))
+    if (option1.checked) {
+        console.log('Option 1 is selected');
+        //repayment type
 
-    console.log(sum) 
-    
+        let monpay = ((principal * (InterestRat * Math.pow(1 + InterestRat, loanTerm))) / (Math.pow(1 + InterestRat, loanTerm) - 1));
+        //let sum = rt((ko(1+ko)**hn)/(((1+ko)**hn)-1))
 
-    
+        let totalrepayment = monpay * loanTerm
+
+        monthrepay.textContent = monpay
+        totalrepay.textContent = totalrepayment
+
+        console.log(totalrepayment)
+        console.log(monpay)
+
+    } else if (option2.checked) {
+        console.log('Option 2 is selected');
+
+        let monpay = principal * InterestRat 
+
+        let totalrepayment = monpay * loanTerm
+
+        monthrepay.textContent = monpay
+        totalrepay.textContent = totalrepayment
+        
+        console.log(totalrepayment)
+        console.log(monpay)
+    } else {
+        console.log('Neither option is selected');
+    }
+
     //interest only type
 }
 
